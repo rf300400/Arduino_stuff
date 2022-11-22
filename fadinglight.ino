a27f207c163edeaea169765517ac8a217eaeb50e
rf300400 <92680754+rf300400@users.noreply.github.com>
2022-11-22 15:06:53 +0530

    Update fadinglight.ino

--- fadinglight.ino
+++ fadinglight.ino
@@ -1,14 +1,7 @@
 int val;
 void setup()
 {
-  pinMode(13,OUTPUT);
-  pinMode(12,OUTPUT);
-  pinMode(11,OUTPUT);
-  pinMode(10,OUTPUT);
   pinMode(9,OUTPUT);
-  pinMode(8,OUTPUT);
-  pinMode(7,OUTPUT);
-  pinMode(6,OUTPUT);
 }
 void loop()
 {
