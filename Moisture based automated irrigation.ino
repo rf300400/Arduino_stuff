8ff61361bcefcbb040b638afc67606c04a33caa8
rf300400 <92680754+rf300400@users.noreply.github.com>
2022-12-07 19:28:55 +0530

    Added commentary.

--- Moisture based automated irrigation.ino
+++ Moisture based automated irrigation.ino
@@ -1,4 +1,5 @@
-short moist = A5, pump = 8 ;
+short moist = A5, pump = 8 ;//Moist is the analog pin taking readings from the moisture sensing module.
+//pump is the digital pin opening and closing the relay as according to the value of moist.
 void setup()
 {
   pinMode(moist,INPUT);
@@ -6,16 +7,14 @@ void setup()
 }
 void loop()
 {
-  if(analogRead(moist) > 400)
+  if(analogRead(moist) > 400)//Lesser the moisture, higher the readings from the moisture module.
   {
     while(analogRead(moist) > 400)
     {
       digitalWrite(pump,HIGH);
-      delay(5000);
-      digitalWrite(pump,LOW);
-    }
-    digitalWrite(pump,LOW);
+    }//Waters the plant until moisture is high enough.
+    digitalWrite(pump,LOW);//Turn off the pump once watering is done.
   }
-  digitalWrite(pump,LOW);
-  delay(20000);
+  digitalWrite(pump,LOW);//Turning off the pump in-cse the pump is on for some reason.
+  delay(20000);Waiting 20 before moisture levels are checked again.
 }
